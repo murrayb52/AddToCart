4a10e43ad991a662b7bdbe6406ab79d9cee4287b
Murray Buchanan <murray.buchanan@mixtelematics.com>
2024-11-20 20:07:59 +0200

    CSS and HTML

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const inputFieldElement = document.getElementById(input-field)
-const addToCartButtonElement = document.getElementById(add-to-cart-button)
+const inputFieldElement = document.getElementById("input-field")
+const addToCartButtonElement = document.getElementById("add-to-cart-button")
 
 addToCartButtonElement.addEventListener("click", function() {
-   let inputFieldValue = inputFieldElement.value
-   console.log(inputFieldValue)
+    let inputValue = inputFieldElement.value
+    
+    console.log(inputValue)
 })
